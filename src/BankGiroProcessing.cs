2d7d60c76684e00db8a8d42258e8507f17c89b3e
Kim Böndergaard <threezool@gmail.com>
2017-03-02 13:11:20 +0100

    Added a new database model to be used for AutoAudit and BankGiroChecker

---
+++ src/BankGiroProcessing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Starcounter;
+
+namespace PresModels
+{
+    [Database]
+    class BankGiroProcessing
+    {
+        public string CompanyName { get; set; }
+        public long OrgNumber { get; set; }
+        public string BGNumber { get; set; }
+        public bool IsProcessed => OrgNumber > 0 && !string.IsNullOrEmpty(CompanyName);
+    }
+}
