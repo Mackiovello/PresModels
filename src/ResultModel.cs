68cb892d940b9a93a4838b33caa66deac234eefa
Erlend <elandroe@icloud.com>
2017-03-02 11:27:21 +0100

    rename to src

---
+++ src/ResultModel.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using Starcounter;
+
+namespace PresModels
+{
+    public enum ResultFlag
+    {
+        Pass,
+        Note,
+        Error
+    }
+
+    [Database]
+    public class ResultModel
+    {
+        public ResultFlag ResultFlag { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public int Account { get; set; }
+        public string Message { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
